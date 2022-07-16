a58d41cc3931ce578353915ebe2fcc0cec85a69d
GW <kowonp@gmail.com>
2022-07-16 17:04:36 +0900

    feat: JSDoc

---
+++ src/myPackage.js
@@ -0,0 +1,20 @@
+// @ts-check
+/**
+ * init은 초기화를 위한 함수입니다.
+ * @param {object} config
+ * @param {boolean} config.debug
+ * @param {string} config.url
+ * @returns {boolean}
+ */
+export function init(config) {
+  return true;
+}
+
+/**
+ * exit은 종료를 위한 함수입니다.
+ * @param {number} code
+ * @returns {number}
+ */
+export function exit(code) {
+  return code + 1;
+}
